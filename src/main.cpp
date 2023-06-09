d26c5999e36b0423e71ed6aad50ea9d279cd72b7
irzazman <irzazman@gmail.com>
2023-06-09 23:17:04 +0800

    test only laa

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,8 @@
 #include <iostream>
-#include ".\include\Membership.h"
 using namespace std;
 
-int main(){
-    char cont = 't';
-    int choice;
-    Membership ms1;
-
-    while (cont=='t')
-    {
-        cout << "Hello World";
-        cont = 'f';
-    }
-
+int main()
+{
+    cout << "I'm under the water";
     return 0;
 }
