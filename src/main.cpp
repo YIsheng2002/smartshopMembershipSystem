10f8518e5cffdaf7f664375cbdd00e8ccab4dd67
yasrizalhakim <88261589+yasrizalhakim@users.noreply.github.com>
2023-06-17 09:42:25 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 //this is just a test to call out the functions (._.
+//later for the menu just call out the function in case1 for add new member and case 2 for displaying the data that is saved in the file.
 
 #include <iostream>
 #include "LinkedList.h"
@@ -7,10 +8,11 @@
 using namespace std;
 
 int main() {
-    LinkedList linkedList;
+    LinkedList linkedList; //LinkedList class
     int choice;
 
-    do {
+    do 
+    {
         cout << "Choose an option:" << endl;
         cout << "1. Add user data" << endl;
         cout << "2. Display user data" << endl;
@@ -18,7 +20,8 @@ int main() {
         cout << "Enter your choice: ";
         cin >> choice;
 
-        switch (choice) {
+        switch (choice) 
+        {
             case 1:
                 addUserData(linkedList);
                 break;
