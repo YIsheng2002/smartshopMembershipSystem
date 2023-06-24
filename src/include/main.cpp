a5e2eb535fb0c1cb4652ffb1a16f3fa5c3d595cf
yasrizalhakim <88261589+yasrizalhakim@users.noreply.github.com>
2023-06-24 13:31:26 +0800

    Update main.cpp

--- src/include/main.cpp
+++ src/include/main.cpp
@@ -7,22 +7,19 @@
 using namespace std;
 
 int main() {
-    MembersData membersData; //MembersData class
+    MembersData membersData;
     int choice;
 
-    do 
-    {
+    do {
         cout << "Choose an option:" << endl;
-        cout << "1. Add new member " << endl;
+        cout << "1. Add new member" << endl;
         cout << "2. Display members data" << endl;
         cout << "3. Exit" << endl;
         cout << "Enter your choice: ";
         cin >> choice;
 
-        switch (choice) 
-        {
+        switch (choice) {
             case 1:
-                membersData.readdatafromfile("membersdata.txt");
                 membersData.addnewmember();
                 membersData.savedatatofile("membersdata.txt");
                 break;
@@ -31,12 +28,12 @@ int main() {
                 membersData.displaydata();
                 break;
             case 3:
-                cout << "bye" << endl;
                 break;
             default:
-                cout << "no option. please try again" << endl;
+                cout << "Invalid option. Please try again." << endl;
                 break;
         }
+
         cout << endl;
     } while (choice != 3);
 
